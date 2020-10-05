887ed7ea276fd391631fafd006b312f2a9543d19
rigel selmani <rigelrigelselmani@hotmail.com>
2020-10-05 02:20:32 -0400

    creatign the input change and click logich as well as importing the input text to the display content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from "./Header"
 import Footer from "./Footer";
 import Create from "./Create";
 import Note from "./Note";
 
 function App() {
+
+  const [newNote,setNewNote]=useState([]);
+
+  function submitNote(notes){
+    setNewNote(prevVal=>{
+      return [...prevVal,notes]
+    })
+  }
   return (
     <div className="App">
       <Header />
-      <Create />
-      <Note />
+      <Create addNote={submitNote} />
+      {newNote.map((text,index) =>{
+        return ( 
+          <Note 
+          key={index}
+          id={index} 
+          title={text.title}
+          content={text.content}/>
+      )})}
       <Footer />
     </div>
   );
-}
+ }
+
 
 export default App;
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Create(){
+function Create(props){
 
     const [note,setNote]=useState({
         title:"",
@@ -8,14 +8,29 @@ function Create(){
     });
 
     function handleChange(event){
-      const inputValue=event.target.value;
-      setNote(inputValue);
+      const {value,name}=event.target;
+      setNote(inputValue=>{
+         return {
+             ...inputValue,
+             [name]:value
+         }
+      });
+    }
+
+    function handleClick(event){
+        props.addNote(note);
+        setNote({
+              title:"",
+              content:""
+        })
+
+        event.preventDefault()
     }
 
     return <div className="Comment">
               <input onChange={handleChange} type="text" placeholder="Title" name="title"/>
               <textarea onChange={handleChange} placeholder="Take a note..." rows="3" />
-              <button>Add</button>
+              <button onClick={handleClick}>Add</button>
            </div>
 }
 
